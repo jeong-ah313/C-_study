bae71806ec4f5a701c5ee62ea6324d4e19fd0a87
JeongahSeong <72932028+jeong-ah313@users.noreply.github.com>
2022-02-07 17:05:02 +0900

    day12

--- MyDiary/mainwindow.cpp
+++ MyDiary/mainwindow.cpp
@@ -6,6 +6,11 @@
 #include <QFile>
 #include <QDir>
 #include <QString>
+#include <QMessageBox>
+#include <QDate>
+#include <QDateTime>
+#include <QComboBox>
+#include <QFileDialog>
 
 //static int sum(int a, int b)
 //{
@@ -17,10 +22,6 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-
-//    ui->pushButton
-
-//    connect(ui->pushButton_7, SIGNAL(clicked()), this, SLOT(selectColorButton())); //수동생성
 }
 
 MainWindow::~MainWindow()
@@ -28,6 +29,7 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+// 원하는 색으로 글자색 변환
 void MainWindow::on_pushButton_7_clicked()
 {
     QColorDialog dlg;
@@ -44,22 +46,15 @@ void MainWindow::on_pushButton_7_clicked()
     }
 }
 
+// txt 파일 불러오기
 void MainWindow::on_pushButton_2_clicked()
 {
-//    QCoreApplication::applicationDirPath() + "/diary.txt"; // 현재 경로에서 test파일을 추가해줌
-
-//    QString file = "../../Desktop/diary.txt";
-
-//    if (QFile::exists(file))
-//    {
-
-//    }
-
-//    ui->textEdit_2->setText();
+    QString filePath = QFileDialog::getOpenFileName(this, "파일 선택", "C:\\Users\\user\\Desktop", "Files(*.txt)");
+    qDebug() << filePath;
 
     // 읽기 파일
-    QFile file("diary.txt");
-    if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
+    QFile file(filePath); // 선택한 파일을 불러오기(읽기)
+    if (!file.open(QIODevice::ReadWrite | QIODevice::Text)) // 파일열기
         return;
 
     QString text;
@@ -67,36 +62,76 @@ void MainWindow::on_pushButton_2_clicked()
     QTextStream in(&file);
     while (!in.atEnd()){ // 파일의 끝이 아니면
         QString line = in.readLine(); // 한줄씨ㄱ 읽는다.
-//        qDebug() << line;
+        //            qDebug() << line;
         text += line + "\n";
     }
 
-//    qDebug() << text;
+
+    QMessageBox::information(this, "title", "Success!");
+
+
+    //    qDebug() << text;
     ui->textEdit_2->setText(text);
 
+    file.close();
+}
 
-    //
+// txt 파일 저장
+void MainWindow::on_pushButton_3_clicked()
+{
+    QString filePath = QFileDialog::getSaveFileName(this, "파일 저장", "C:\\Users\\user\\Desktop", "Files(*.txt)"); // 현재 문서를 저장할 파일을 선택
+    //    qDebug() << File;
 
-//    QDir Directory("Diary"); // 파일 폴더 선택
-//    if(!Directory.exists()) // 폴더 존재 유무 확인
-//    {
-//        Directory.mkdir("/."); // 없으면 현재 위치에 폴더 생성
-//    }
+    // 저장 파일
+    QFile output(filePath);
+    if (!output.open(QIODevice::ReadWrite | QIODevice::Text))
+        return;
+
+    QString TextOut;
+    QTextStream out(&output); // output위치의 포인터를 out이라 정함
+
+    const auto text = ui->textEdit_2->toPlainText();
+
+    out << text;
+
+    output.close();
+
+    QMessageBox::information(this, "title", "Success!");
+}
 
 
-//    QDir Directory("Diary"); // 파일 폴더 선택
+void MainWindow::on_pushButton_clicked()
+{
+    ui->textEdit_2->clear();
+    // 지울게 없을ㄸㅐ는 따로 다른 메세지창을 띄우게 하는 게 좋을 것 같음
+    QMessageBox::information(this, "title", "Clear!");
+
+}
+
+// 날짜
+void MainWindow::on_pushButton_4_clicked()
+{
+
+    QDateTime time= QDateTime::currentDateTime();
+    QString time_text = time.toString("yyyy - MM - dd"); // lineEdit에 담을거라 \n가 안먹음
+
+    ui->lineEdit->setText(time_text);
+
+}
+
+// 시간
+void MainWindow::on_pushButton_8_clicked()
+{
+    QTime time= QTime::currentTime();
+    QString time_text = time.toString("hh : mm : ss"); // lineEdit에 담을거라 \n가 안먹음
+
+    ui->lineEdit_2->setText(time_text);
+}
 
 
-//    QFile File("test.txt");
-
-//    // 읽기 전용
-//    if(!File.open(QFile::ReadOnly::Text))
-//    {
-//        if(!File.exists())
-//        {
-
-//        }
-//    }
-
+void MainWindow::on_textEdit_2_textChanged()
+{
+    const auto text = ui->textEdit_2->toPlainText();
+    ui->plainTextEdit_2->document()->setPlainText(QString::number(text.length()));
 }
 
